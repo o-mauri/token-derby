d00f46f94ea028ea56ce0d6dbe7ff62926342716
OMauriStkOne <omar@stackone.com>
2026-06-02 12:23:31 +0100

    feat(infra): wire public org leaderboard route

--- infra/lib/token-derby-stack.ts
+++ infra/lib/token-derby-stack.ts
@@ -106,6 +106,7 @@ export class TokenDerbyStack extends cdk.Stack {
     const listOrgsFn = makeFn('ListOrgsFn', 'list-organisations');
     const getOrgFn = makeFn('GetOrgFn', 'get-organisation');
     const listOrgRacesFn = makeFn('ListOrgRacesFn', 'list-org-races');
+    const getOrgLeaderboardFn = makeFn('GetOrgLeaderboardFn', 'get-org-leaderboard');
     const setOrgWebhookFn = makeFn('SetOrgWebhookFn', 'set-org-webhook');
     const getOrgWebhookFn = makeFn('GetOrgWebhookFn', 'get-org-webhook');
     const deleteOrgWebhookFn = makeFn('DeleteOrgWebhookFn', 'delete-org-webhook');
@@ -139,6 +140,7 @@ export class TokenDerbyStack extends cdk.Stack {
     httpApi.addRoutes({ path: '/api/organisations/join', methods: [HttpMethod.POST], integration: new HttpLambdaIntegration('JoinOrgInt', joinOrgFn) });
     httpApi.addRoutes({ path: '/api/organisations/{org_name}', methods: [HttpMethod.GET], integration: new HttpLambdaIntegration('GetOrgInt', getOrgFn) });
     httpApi.addRoutes({ path: '/api/organisations/{org_name}/races', methods: [HttpMethod.GET], integration: new HttpLambdaIntegration('ListOrgRacesInt', listOrgRacesFn) });
+    httpApi.addRoutes({ path: '/api/organisations/{org_name}/leaderboard', methods: [HttpMethod.GET], integration: new HttpLambdaIntegration('GetOrgLeaderboardInt', getOrgLeaderboardFn) });
     httpApi.addRoutes({
       path: '/api/organisations/{org_name}/webhook',
       methods: [HttpMethod.PUT],
